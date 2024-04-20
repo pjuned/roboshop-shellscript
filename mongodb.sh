6feeb0ec7a32e872ccde3f7221be90468b439241
junaid <pjuned.it@gmail.com>
2024-04-20 20:42:54 +0530

    pushing

---
+++ mongodb.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ID=$(id -u)
+
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+TIMESTAMP=$(date +%F-%H-%M-%S)
+LOGFILE="/tmp/$0-$TIMESTAMP.log"
+
+echo -e "script started executing at $TIMESTAMP" &>> $LOGFILE
+
+VALIDATE() {
+    if [ $1 -ne 0 ]
+    then
+        echo -e  "$2 is.. $R failed $N "
+    else
+        echo -e  "$2 is..$G Success $N "
+    fi
+            }
+    if [ $ID -ne 0 ]
+then
+    echo -e "$R you are not a root user $N"
+    exit 1
+else
+    echo "you are a root user"
+fi
+
+cp mongo.repo /etc/yum.repos.d/mongo.repo &>> $LOGFILE
+
+VALIDATE $? "Copied Mongodb Repo"
+
